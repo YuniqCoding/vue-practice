7edb709b7d647f2ca87d0dc673a27fc073be3406
Yura <rkdldk37@gmail.com>
2024-06-03 17:53:47 +0900

    '20240603update'

--- chapter10/router-test/src/router/index.js
+++ chapter10/router-test/src/router/index.js
@@ -4,6 +4,7 @@ import Home from '@/pages/Home.vue';
 import About from '@/pages/About.vue';
 import Members from '@/pages/Members.vue';
 import Videos from '@/pages/Videos.vue';
+import NotFound from '@/pages/NotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -31,6 +32,13 @@ const router = createRouter({
     {
       path: '/videos',
       component: Videos,
+    },
+    {
+      // 어떤 문자든지 위에서 해당하지 않는 경로는 다 여기서 처리
+      // 404 라우트
+      path: '/:paths(.*)*',
+      name: 'NotFound',
+      component: NotFound,
     },
   ],
 });
